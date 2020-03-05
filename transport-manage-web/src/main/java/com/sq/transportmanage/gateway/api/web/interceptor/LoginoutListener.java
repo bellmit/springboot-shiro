defeb58509b8cf421e9a04494db743af3d566a9e
fanhongtao <fanhongtao@01zhuanche.com>
2020-03-05 14:52:53 +0800

    修改正则匹配名称

---
+++ transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/interceptor/LoginoutListener.java
@@ -0,0 +1,29 @@
+package com.sq.transportmanage.gateway.api.web.interceptor;
+
+import com.google.common.collect.Maps;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.Map;
+
+/**
+ * @Author fanht
+ * @Description 监听当前有哪些用户，当收到特定通知后通知退出登录
+ * @Date 2020/3/5 下午1:48
+ * @Version 1.0
+ */
+@Component
+public class LoginoutListener implements HttpSessionListener{
+
+    public static Map<Integer,String> mapUser = Maps.newHashMap();
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+    }
+}
