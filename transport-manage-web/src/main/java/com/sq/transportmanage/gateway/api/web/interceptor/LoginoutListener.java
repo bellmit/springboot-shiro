f845975336da3579b287c9dbadbd3463c2680a0d
fanhongtao <fanhongtao@01zhuanche.com>
2020-03-06 10:51:28 +0800

    添加shiro的监听器解决修改菜单用户自动退出

--- transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/interceptor/LoginoutListener.java
+++ transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/interceptor/LoginoutListener.java
@@ -1,11 +1,17 @@
 package com.sq.transportmanage.gateway.api.web.interceptor;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Maps;
+import com.sq.transportmanage.gateway.service.common.shiro.session.RedisSessionDAO;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
+import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author fanht
@@ -14,16 +20,52 @@ import java.util.Map;
  * @Version 1.0
  */
 @Component
-public class LoginoutListener implements HttpSessionListener  {
+public class LoginoutListener extends RedisSessionDAO   implements SessionListener {
 
-    public static Map<Integer,String> mapUser = Maps.newHashMap();
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    public  static final Map<Long,String> mapUser = Maps.newHashMap();
+    public final static AtomicInteger sessionCount = new AtomicInteger(0);
 
     @Override
-    public void sessionCreated(HttpSessionEvent se) {
+    public void onStart(Session session) {
+        //会话创建，在线人数加一
+        logger.info("======" + sessionCount);
+        sessionCount.incrementAndGet();
+    }
 
+    @Override
+    public void onStop(Session session) {
+        //会话退出,在线人数减一
+        sessionCount.decrementAndGet();
+    }
+
+    @Override
+    public void onExpiration(Session session) {
+        //会话过期,在线人数减一
+        sessionCount.decrementAndGet();
+
+    }
+
+
+    /**
+     * 获取在线人数使用
+     * @return
+     */
+    public AtomicInteger getSessionCount() {
+        return sessionCount;
+    }
+
+
+
+    /*@Override
+    public void sessionCreated(HttpSessionEvent se) {
+        onlineCount++;
+        logger.info("创建start====== ===" + se.getSession().getId());
+        mapUser.put(se.getSession().getCreationTime(),se.getSession().getId());
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-    }
+        logger.info("销毁session=============");
+    }*/
 }
