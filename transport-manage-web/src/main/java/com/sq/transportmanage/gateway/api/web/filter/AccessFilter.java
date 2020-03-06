998873a01188b6b78774b1529f6afc90ce1635e9
zhaojingwei <zhaojingwei@01zhuanche.com>
2020-03-06 14:42:10 +0800

    上传文件

--- transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/filter/AccessFilter.java
+++ transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/filter/AccessFilter.java
@@ -46,7 +46,7 @@ public class AccessFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
         logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-        if(request.getRequestURL().toString().contains("/common/upload/")){
+        if(request.getRequestURL().toString().contains("/common/upload")){
             request.setAttribute("filePath","driverspark/");
         }
 //        SSOLoginUser loginUser = WebSessionUtil.getCurrentLoginUser();
