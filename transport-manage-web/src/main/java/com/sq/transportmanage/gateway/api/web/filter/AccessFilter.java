d5633e06597a14fe821b62ce33e25705c2d4635f
zhaojingwei <zhaojingwei@01zhuanche.com>
2020-03-06 16:51:33 +0800

    权限拦截

--- transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/filter/AccessFilter.java
+++ transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/filter/AccessFilter.java
@@ -46,34 +46,33 @@ public class AccessFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
         logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-        if(request.getRequestURL().toString().contains("/common/upload")){
-            request.setAttribute("filePath","driverspark/");
+        SSOLoginUser loginUser = WebSessionUtil.getCurrentLoginUser();
+        logger.info(String.format("%s loginUser %s", request.getMethod(), loginUser.getName()));
+        /**用户是否有权限**/
+        boolean bl = false;
+        //如果是管理员 直接通过
+        if(AuthEnum.MANAGE.getAuthId().equals(loginUser.getAccountType())){
+            bl = true;
+        }else {
+            String uri = request.getRequestURI().toString();
+            List<String> menuUrl = loginUser.getMenuUrlList();
+            if(menuUrl.contains(uri)){
+                bl = true;
+            }
         }
-//        SSOLoginUser loginUser = WebSessionUtil.getCurrentLoginUser();
-//        logger.info(String.format("%s loginUser %s", request.getMethod(), loginUser.getName()));
-//        /**用户是否有权限**/
-//        boolean bl = false;
-//        //如果是管理员 直接通过
-//        if(AuthEnum.MANAGE.getAuthId().equals(loginUser.getAccountType())){
-//            bl = true;
-//        }else {
-//            String uri = request.getRequestURI().toString();
-//            List<String> menuUrl = loginUser.getMenuUrlList();
-//            if(menuUrl.contains(uri)){
-//                bl = true;
-//            }
-//        }
-//        if(bl){
-//            JSONObject data = new JSONObject();
-//            data.put("sysId","t_saas");
-//            data.put("account",loginUser.getLoginName());
-//            ctx.addZuulRequestHeader("login_user",data.toJSONString());
-//        }else{
-//            ctx.setSendZuulResponse(false);// 过滤该请求，不对其进行路由
-//            ctx.setResponseStatusCode(401);// 返回错误码
-//            ctx.setResponseBody("{\"code\":0,\"result\":\"网关验证失败!验证方式为2\"}");// 返回错误内容
-//            ctx.set("isSuccess", false);
-//        }
+        if(bl){
+            JSONObject data = new JSONObject();
+            data.put("sysId","t_saas");//平台ID
+            data.put("merchantId",loginUser.getMerchantId());//商户ID
+            data.put("account",loginUser.getLoginName());//用户名
+            data.put("name",loginUser.getName());//用户名中文
+            ctx.addZuulRequestHeader("login_user",data.toJSONString());
+        }else{
+            ctx.setSendZuulResponse(false);// 过滤该请求，不对其进行路由
+            ctx.setResponseStatusCode(401);// 返回错误码
+            ctx.setResponseBody("{\"code\":0,\"result\":\"网关验证失败!请先登录\"}");// 返回错误内容
+            ctx.set("isSuccess", false);
+        }
         return ctx;
     }
 
