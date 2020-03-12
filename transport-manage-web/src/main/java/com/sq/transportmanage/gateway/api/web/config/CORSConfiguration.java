8d4ae28b8a1b840b997ccecdf05ad1a2587285eb
zhaojingwei <zhaojingwei@01zhuanche.com>
2020-03-12 22:37:47 +0800

    /car/batchImport

--- transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/config/CORSConfiguration.java
+++ transport-manage-web/src/main/java/com/sq/transportmanage/gateway/api/web/config/CORSConfiguration.java
@@ -7,9 +7,11 @@ package com.sq.transportmanage.gateway.api.web.config;
  * @create: 2020-03-12 21:22
  **/
 
+import com.sq.transportmanage.gateway.api.web.interceptor.HttpParamVerifyInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,21 +19,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * 解决跨域问题springboot所需配置
  */
 @Configuration
-public class CORSConfiguration {
-    @Bean
-    public WebMvcConfigurer CORSConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        //设置是否允许跨域传cookie
-                        .allowCredentials(true)
-                        //设置缓存时间，减少重复响应
-                        .maxAge(3600);
-            }
-        };
+public class CORSConfiguration  extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new CharacterValidInterceptors()).addPathPatterns("/**");
+        registry.addInterceptor(new HttpParamVerifyInterceptor()).addPathPatterns("/**");
+        super.addInterceptors(registry);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
+
 }
